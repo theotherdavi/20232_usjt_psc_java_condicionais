3f55f712b960ebfdeef7a558b0edc7cf3f6dc6ca
Davi Batista <davideosmar11@gmail.com>
2023-09-12 21:12:30 -0300

    ap03(java): added accumulator

--- NumbersUntilZero.java
+++ NumbersUntilZero.java
@@ -4,11 +4,15 @@ public class NumbersUntilZero {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Insira um número, digite 0 para finalizar:\n");
+        int acc = 0;
         int number = scanner.nextInt();
+        acc += number;
         while (number != 0) {
+            System.out.print("Próximo: 1");
             number = scanner.nextInt();
+            acc += number;
         }
-        System.out.println("Finalizando...");
+        System.out.println("Valor final: " + acc);
 
         scanner.close();
     }
